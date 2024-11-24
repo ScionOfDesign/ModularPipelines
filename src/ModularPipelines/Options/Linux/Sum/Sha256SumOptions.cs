9d8948ded18b39fe2a4682b2e3165c254840d093
Scion <scion@studiowhy.net>
2024-11-24 11:43:25 -0800

    Added Sha256um.

---
+++ src/ModularPipelines/Options/Linux/Sum/Sha256SumOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace ModularPipelines.Options.Linux.Sum;
+
+public sealed record Sha256SumOptions : SumOptions
+{
+    public Sha256SumOptions(params string[] filenames) : base("sha256sum", filenames)
+    {
+    }
+}
---
+++ src/ModularPipelines/Options/Linux/Sum/SumOptions.cs
@@ -0,0 +1,44 @@
+﻿using ModularPipelines.Attributes;
+
+namespace ModularPipelines.Options.Linux.Sum;
+
+public abstract record SumOptions : CommandLineToolOptions
+{
+    protected SumOptions(string tool, params string[] filenames) : base(tool)
+    {
+        Filenames = filenames;
+    }
+
+    [BooleanCommandSwitch("-b")]
+    public bool? Binary { get; set; }
+
+    [BooleanCommandSwitch("-c")]
+    public bool? Check { get; set; }
+
+    [BooleanCommandSwitch("-t")]
+    public bool? Text { get; set; }
+
+    [BooleanCommandSwitch("-z")]
+    public bool? Zero { get; set; }
+
+    [BooleanCommandSwitch("--tag")]
+    public bool? Tag { get; set; }
+
+    [BooleanCommandSwitch("--ignore-missing")]
+    public bool? IgnoreMissing { get; set; }
+
+    [BooleanCommandSwitch("--quiet")]
+    public bool? Quiet { get; set; }
+
+    [BooleanCommandSwitch("--status")]
+    public bool? Status { get; set; }
+
+    [BooleanCommandSwitch("--strict")]
+    public bool? Strict { get; set; }
+
+    [BooleanCommandSwitch("--warn")]
+    public bool? Warn { get; set; }
+
+    [PositionalArgument(Position = Position.AfterSwitches)]
+    public IEnumerable<string> Filenames { get; set; }
+}
